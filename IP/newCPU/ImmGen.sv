56d0600e9b801362d5783388a982a5a720128e9c
Liao Junxuan <mikeljx@126.com>
2023-03-18 15:40:15 +0800

    refactor: use systemverilog-style sign extension

--- IP/newCPU/ImmGen.sv
+++ IP/newCPU/ImmGen.sv
@@ -10,30 +10,31 @@ module ImmGen
     logic unsign,
     output DType immout
 );
+    localparam width = $bits(DType);
     always_comb begin
         unique case (itype)
             // I8 must sll/sra/srl , unsigned extend
             I8: begin
-                immout = $bits(DType)'(instr[14:10]);
+                immout = width'(instr[14:10]);
             end
             I12: begin
-                if (unsign) immout = $bits(DType)'(instr[21:10]);
-                else immout = {{20{instr[21]}}, instr[21:10]};
+                if (unsign) immout = width'(instr[21:10]);
+                else immout = width'(signed'(instr[21:10]));
             end
             I14: begin
-                immout = {{16{instr[23]}}, instr[23:10], 2'b0};
+                immout = width'(signed'({instr[23:10], 2'b0}));
             end
             I16: begin
-                immout = {{14{instr[25]}}, instr[25:10], 2'b0};
+                immout = width'(signed'({instr[25:10], 2'b0}));
             end
             I20: begin
                 immout = {instr[24:5], 12'b0};
             end
             I21: begin
-                immout = {{11{instr[4]}}, instr[4:0], instr[25:10]};
+                immout = width'(signed'({instr[4:0], instr[25:10]}));
             end
             I26: begin
-                immout = {{4{instr[9]}}, instr[9:0], instr[25:10], 2'b0};
+                immout = width'(signed'({instr[9:0], instr[25:10], 2'b0}));
             end
             default: begin
                 immout = '0;
